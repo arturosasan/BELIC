d3b53723ff0f91853d665236514cd7ea1620a735
asarsan <asarsan@etsinf.upv.es>
2025-08-16 00:48:52 +0200

    Arreglo colores días + pruebas

--- src/main.js
+++ src/main.js
@@ -7,12 +7,12 @@ let calendarEl = document.getElementById('calendar');
 let calendar = new Calendar(calendarEl, {
   plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
   initialView: 'dayGridMonth',
-  locale: 'es', // En español, pero sin mayus (¿?)
+  locale: 'es', // En español
   firstDay: 1, // Empieza en Lunes
   headerToolbar: {
-    start: 'prev',
-    center: 'title',
-    end: 'next'
+    start: 'prev', // IZQ
+    center: 'title', // CENTRO
+    end: 'next', // DER
     },
     titleFormat:{
       year: 'numeric', 
@@ -39,12 +39,13 @@ function generateEvents(fechaInicio, fechaFin, diasSemana, color) {
   return eventos;
 }
 
-const hoy = new Date();
-const fin = new Date(hoy.getFullYear() + 1, hoy.getMonth(), hoy.getDate());
+//const hoy = new Date();
+const hoy = new Date(2025, 8, 2); // EMPIEZA EL 1 DE SEPTIEMBRE DEL 2025
+const fin = new Date(hoy.getFullYear() + 1, hoy.getMonth(), hoy.getDate()); // FIN DEL PERIODO (EN ESTE CASO 1 DE SEPTIEMBRE DEL 2026) (CONST = HOY)
 
-const weekly = generateEvents(hoy, fin, [1, 2, 3, 4, 5], 'red');
-const saturday      = generateEvents(hoy, fin, [6], 'yellow');
-const sunday     = generateEvents(hoy, fin, [0], 'blue');
+const weekly = generateEvents(hoy, fin, [2, 3, 4, 5, 6], 'red'); // Días de semana
+const saturday      = generateEvents(hoy, fin, [0], 'yellow'); // Sábados
+const sunday     = generateEvents(hoy, fin, [1], 'blue'); // Domingos
 
 const allEvents = [
   ...weekly,
